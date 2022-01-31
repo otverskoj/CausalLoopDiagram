c7b9da741618174724e8803364b67a0bf117ae70
Oleg Tverskoy <otverskoj@gmail.com>
2022-01-31 13:18:14 +0600

    Add synonyms lists for the verbs "increase" and "decrease"

---
+++ verbs.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+VERBS_INCREASE = np.array((
+    'increase', 'advance', 'boost', 'broaden', 'build', 'build up', 'deepen',
+    'develop', 'double', 'enhance', 'enlarge', 'escalate', 'expand', 'extend',
+    'further', 'heighten', 'intensify', 'multiply', 'raise', 'reinforce',
+    'rise', 'step up', 'strengthen', 'swell', 'triple', 'widen', 'aggrandize',
+    'aggravate', 'amplify', 'annex', 'augment', 'dilate', 'distend',
+    'exaggerate', 'inflate', 'lengthen', 'magnify', 'mount', 'pad', 'progress',
+    'proliferate', 'prolong', 'protract', 'pullulate', 'redouble', 'sharpen',
+    'snowball', 'spread', 'supplement', 'swarm', 'teem', 'thicken', 'wax',
+    'mark up', 'slap on', 'tack on'
+))
+VERBS_DECREASE = np.array((
+    'abate', 'curb', 'curtail', 'cut down', 'decline', 'depreciate',
+    'deteriorate', 'diminish', 'drop', 'drop off', 'dwindle', 'ease', 'ebb',
+    'fall off', 'lessen', 'lower', 'reduce', 'shrink', 'sink', 'slacken',
+    'slash', 'slump', 'soften', 'subside', 'wane', 'weaken', 'check',
+    'contract', 'crumble', 'decay', 'degenerate', 'devaluate', 'droop',
+    'evaporate', 'fade', 'lighten', 'modify', 'quell', 'quiet', 'restrain',
+    'settle', 'shrivel', 'waste', 'wither', 'calm down', 'die down', 'dry up',
+    'let up', 'lose edge', 'narrow down', 'peter out', 'run low', 'slack off',
+    'slow down', 'tail off', 'wear away'
+))
+    
