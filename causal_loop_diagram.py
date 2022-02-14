1b95a3d9b86a63bb12060c577009eba1d95175f8
Oleg Tverskoy <otverskoj@gmail.com>
2022-02-14 13:06:07 +0600

    Add ability to save diagram image

--- causal_loop_diagram.py
+++ causal_loop_diagram.py
@@ -11,12 +11,21 @@ def make_diagram(text: str) -> nx.DiGraph:
     return merge_graphs(make_subgraph_list(text))
 
 
-def draw_diagram(digraph: nx.DiGraph, pos: Optional[dict] = None):
+def draw_diagram(digraph: nx.DiGraph, pos: Optional[dict] = None) -> None:
+    _draw_helper(digraph, pos)
+    plt.show()
+
+
+def save_diagram_image(digraph: nx.DiGraph, filepath: str, pos: Optional[dict] = None) -> None:
+    _draw_helper(digraph, pos)
+    plt.savefig(filepath)
+
+
+def _draw_helper(digraph: nx.DiGraph, pos: Optional[dict] = None) -> None:
     pos = pos or nx.spring_layout(digraph)
     edge_labels = _get_edge_labels(digraph)
     nx.draw_networkx(digraph, pos=pos)
     nx.draw_networkx_edge_labels(digraph, pos=pos, edge_labels=edge_labels)
-    plt.show()
 
 
 def _get_edge_labels(digraph: nx.DiGraph) -> dict:
